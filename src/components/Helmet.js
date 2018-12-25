f7984e3f60f37791be37091bca63978f64383ada
TIMOTHY R. BULA <trbula@us.ibm.com>
2018-12-25 01:07:53 -0600

    fix: icons and title

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -11,6 +11,7 @@ const Helmet = ({ theme = {} }) => (
         contentfulAbout {
           name
           description
+          appTitle
           profile {
             favicon16: resize(width: 16) {
               src
@@ -29,51 +30,36 @@ const Helmet = ({ theme = {} }) => (
       }
     `}
     render={data => {
-      const { name, description, profile } = data.contentfulAbout;
-      const title = `${name} Portofolio`;
+      const { description, profile, appTitle } = data.contentfulAbout;
 
       return (
         <ReactHelmet>
           <meta charSet="utf-8" />
-          <title>{title}</title>
+          <title>{appTitle}</title>
           <meta name="description" content={description} />
           <link rel="shortcut icon" href={profile.favicon32.src} />
           <meta name="theme-color" content={theme.background} />
           <meta name="image" content={profile.favicon32.src} />
 
-          <meta itemProp="name" content={title} />
+          <meta itemProp="name" content={appTitle} />
           <meta itemProp="description" content={description} />
           <meta itemProp="image" content={profile.favicon32.src} />
 
-          <meta name="og:title" content={title} />
+          <meta name="og:title" content={appTitle} />
           <meta name="og:description" content={description} />
           <meta name="og:image" content={profile.bigIcon.src} />
-          <meta name="og:site_name" content={title} />
+          <meta name="og:site_name" content={appTitle} />
           <meta name="og:locale" content="en_US" />
           <meta name="og:type" content="website" />
 
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content={title} />
+          <meta name="twitter:title" content={appTitle} />
           <meta name="twitter:description" content={description} />
           <meta name="twitter:image" content={profile.bigIcon.src} />
 
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href={profile.appleIcon.src}
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href={profile.favicon32.src}
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href={profile.favicon16.src}
-          />
+          <link rel="apple-touch-icon" sizes="180x180" href={profile.appleIcon.src} />
+          <link rel="icon" type="image/png" sizes="32x32" href={profile.favicon32.src} />
+          <link rel="icon" type="image/png" sizes="16x16" href={profile.favicon16.src} />
 
           <link
             href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
